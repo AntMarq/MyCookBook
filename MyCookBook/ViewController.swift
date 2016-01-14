da15dec5a1abf115bcadc59d373687fbaa5bea39
Marquet Anthony <amarquet@sqli.com>
2016-01-14 18:59:15 +0100

    add images and start segue

--- MyCookBook/ViewController.swift
+++ MyCookBook/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "HomeToRecipe"
+        {
+            if let destinationVC = segue.destinationViewController as? RecipeViewController{
+               // destinationVC.numberToDisplay = counter
+            }
+        }
     }
 
 
