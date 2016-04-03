a6b05dadd50a3e5d1119975459a11693c193d5cd
anthony <anthonymarquet21@gmail.com>
2016-04-03 21:38:16 +0200

    Init iCarousel

--- MyCookBook/MainViewController.swift
+++ MyCookBook/MainViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 private let segueID = "HomeToRecipesList"
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
     
     var imgID = 0
     let titleImgEntree:String = "Mes Entrées"
     let titleImgPlat:String = "Mes Plats Principaux"
     let titleImgDessert:String = "Mes Desserts"
+    var categoryList:[UIImage] = []
     
+    @IBOutlet weak var carousel: iCarousel!
     @IBOutlet weak var imgEntree: UIImageView!
     @IBOutlet weak var imgDessert: UIImageView!
     @IBOutlet weak var imgPlat: UIImageView!
@@ -32,11 +34,8 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        addGestureToImageView(imgEntree)
-        addGestureToImageView(imgDessert)
-        addGestureToImageView(imgApero)
-        addGestureToImageView(imgPlat)
-        
+        categoryList = [imgEntree.image!,imgDessert.image!, imgApero.image!,imgPlat.image!]
+        carousel.type = iCarouselType.Rotary
         AlamofireManager.SharedInstance.getToken { (success) -> Void in
             if(!success){
                 print("No token")
@@ -91,15 +90,61 @@ class MainViewController: UIViewController {
     }
     
     
-    func addGestureToImageView(img:UIImageView){
+   /* func addGestureToImageView(img:UIImageView){
         let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
         img.addGestureRecognizer(tapGestureRecognizer);
-    }
+    }*/
     
     func backController(){
         navigationController?.popViewControllerAnimated(true)
     }
     
+    func numberOfItemsInCarousel(carousel: iCarousel) -> Int
+    {
+        return 4
+    }
+    
+    func carousel(carousel: iCarousel, viewForItemAtIndex index: Int, reusingView view: UIView?) -> UIView
+    {
+        var itemView: UIImageView
+        
+        //create new view if no view is available for recycling
+        if (view == nil)
+        {
+            //don't do anything specific to the index within
+            //this `if (view == nil) {...}` statement because the view will be
+            //recycled and used with other index values later
+            itemView = UIImageView(frame:CGRect(x:0, y:0, width:350, height:280))
+            itemView.image = self.categoryList[index]
+            itemView.contentMode = .ScaleToFill
+            itemView.tag = index
+        }
+        else
+        {
+            //get a reference to the label in the recycled view
+            itemView = view as! UIImageView;
+        }
+        
+        //set item label
+        //remember to always set any properties of your carousel item
+        //views outside of the `if (view == nil) {...}` check otherwise
+        //you'll get weird issues with carousel item content appearing
+        //in the wrong place in the carousel
+        
+        return itemView
+    }
+    
+    func carousel(carousel: iCarousel, valueForOption option: iCarouselOption, withDefault value: CGFloat) -> CGFloat
+    {
+        if (option == .Spacing)
+        {
+            return value * 3
+        }
+        return value
+    }
+    
     
 }
+
+
 
